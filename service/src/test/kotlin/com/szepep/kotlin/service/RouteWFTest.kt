8bc2c5ebe6a180d269f6c5b0d68aca3465d2698b
Peter Szepe <szepep@gmail.com>
2021-09-06 19:58:40 +0200

    Add test with WebFluxTest

---
+++ service/src/test/kotlin/com/szepep/kotlin/service/RouteWFTest.kt
@@ -0,0 +1,35 @@
+package com.szepep.kotlin.service
+
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Test
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.autoconfigure.web.reactive.WebFluxTest
+import org.springframework.context.annotation.Import
+import org.springframework.test.web.reactive.server.WebTestClient
+import org.springframework.test.web.reactive.server.expectBody
+
+@WebFluxTest
+@Import(
+    RouterConfiguration::class,
+    RequestHandler::class,
+    GeneratorImpl::class,
+)
+internal class RouteWFTest {
+
+    @Autowired
+    private lateinit var client: WebTestClient
+
+    @Test
+    fun `test router with length 10`() {
+        client.get().uri("/generate/10/10").exchange()
+            .expectStatus().isOk
+            .expectBody<String>().consumeWith { assertEquals(10, it.responseBody?.length) }
+    }
+
+    @Test
+    fun `test router with length 100`() {
+        client.get().uri("/generate/10/100").exchange()
+            .expectStatus().isOk
+            .expectBody<String>().consumeWith { assertEquals(100, it.responseBody?.length) }
+    }
+}
